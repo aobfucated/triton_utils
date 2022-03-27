fee2fd2056dfb398e0fd9010010de9aacffc5c48
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-03-27 10:58:50 +0800

    updadte

--- triton_utils.hpp
+++ triton_utils.hpp
@@ -1,1 +1,15 @@
+#pragma once 
+#include<vector>
+namespace ttutils
+{
 
+	uint64_t to_qword(const std::vector<uint8_t>& v)
+	{
+		assert(v.size() == 8);
+		uint64_t t = 0;
+		memcpy(&t, v.data(), 8);
+		return t;
+	}
+
+
+}
