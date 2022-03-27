d0198d748792cc7ec8bf1c24af80c29530d0d72c
极诣·剑魂 <84119280+helloobaby@users.noreply.github.com>
2022-03-27 11:01:32 +0800

    updadte

--- triton_utils.hpp
+++ triton_utils.hpp
@@ -11,5 +11,22 @@ namespace ttutils
 		return t;
 	}
 
+	uint32_t to_dword(const std::vector<uint8_t>& v)
+	{
+		assert(v.size() == 4);
+		uint32_t t = 0;
+		memcpy(&t, v.data(), 4);
+		return t;
+	}
+
+	uint16_t to_word(const std::vector<uint8_t>& v)
+	{
+		assert(v.size() == 2);
+		uint16_t t = 0;
+		memcpy(&t, v.data(), 2);
+		return t;
+	}
+
+		
 
 }
